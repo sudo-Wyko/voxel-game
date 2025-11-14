7b1bd70ece2ed95746678a281cba57d2e84154dc
Wyko <nwortula@gmail.com>
2025-11-14 22:12:29 +0800

    Initial commit - got a window running

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Voxel Game", NULL, NULL);
+    if(window == NULL) 
+    {
+        std::cout << "Failed to create GLFW window";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+    {
+        std::cout << "Failed to initialize GLAD";
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
